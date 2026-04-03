d1d765030ec8105c8ee31a7f45c6f6db6e518c62
tomas <tom4s.fr4ncisco@gmail.com>
2026-04-03 20:13:54 +0100

    Updated be url

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_BASE = "http://localhost:8080/api";
+const API_BASE = "https://easter-be-2.onrender.com";
 
 interface Clue {
     id?: number;
